4033f0c1499b9239f1db128094e7a1710c9d437b
Axel Cedercreutz <axel.cedercreutz@gmail.com>
2020-09-23 11:21:42 +0300

    all fixes, heroku left to fix

--- osa3/backend/src/index.js
+++ osa3/backend/src/index.js
@@ -114,6 +114,6 @@ const errorHandler = (error, request, response, next) => {
 
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
